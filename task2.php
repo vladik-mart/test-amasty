ad8ca9476bfa5f520ff0eb96fc92e86782b36f05
vladik-mart <70260002+vladik-mart@users.noreply.github.com>
2020-08-26 11:45:00 -0400

    Removed useless files and add task 2

---
+++ task2.php
@@ -0,0 +1,51 @@
+<?php
+
+class StroopTest
+{
+    const WORDS = ['red', 'blue', 'green', 'yellow', 'lime', 'magenta', 'black', 'gold', 'gray', 'tomato'];
+
+    private $rows;
+    private $cols;
+
+    public function __construct($rows = 5, $cols = 5)
+    {
+        $this->rows = $rows;
+        $this->cols = $cols;
+    }
+
+    private function Row()
+    {
+        $row = [];
+        $words = self::WORDS;
+        $rows = $this->rows;
+        while ($rows > 0) {
+            $indexes = array_rand($words, 2);
+            $row[] = [
+                $words[$indexes[0]],
+                $words[$indexes[1]]
+            ];
+            unset($words[$indexes[0]], $words[$indexes[1]]);
+            $rows--;
+        }
+
+        return $row;
+    }
+
+    public function __toString()
+    {
+        $output = '';
+        $cols = $this->cols;
+        while ($cols > 0) {
+            foreach ($this->Row() as $r) {
+                $output .= '<i style="color: ' . $r[0] . '">' . $r[1] . ' </i>';
+            }
+            $output .= '<br>';
+            $cols--;
+        }
+
+        return $output;
+    }
+}
+
+echo new StroopTest();
+
